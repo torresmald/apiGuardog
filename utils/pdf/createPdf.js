98fd0a0e57552ada4cc77629362fcb27d424341f
torresmald <jonathan.torresmald@gmail.com>
2024-06-12 10:00:20 +0200

    path

--- utils/pdf/createPdf.js
+++ utils/pdf/createPdf.js
@@ -13,70 +13,74 @@ export const createPDF = (name, appointment, filePath) => {
 
         doc.pipe(stream);
 
-        const startX = 100; // Posición X inicial para el texto
-        const startY = 100; // Posición Y inicial
-        const imageWidth = 100; // Ancho de la imagen
-        const imageX = 400; // Posición X para la imagen
+        // const startX = 100; // Posición X inicial para el texto
+        // const startY = 100; // Posición Y inicial
+        // const imageWidth = 100; // Ancho de la imagen
+        // const imageX = 400; // Posición X para la imagen
 
-        const imagePath = path.resolve(__dirname, './logo.jpeg');
+        // const imagePath = path.resolve(__dirname, './logo.jpeg');
 
-        // Añadir imagen
-        doc.image(imagePath, imageX, startY, { width: imageWidth });
+        // // Añadir imagen
+        // doc.image(imagePath, imageX, startY, { width: imageWidth });
 
         // Añadir texto
-        const textX = startX;
-        doc.fontSize(27).text('Factura Guardog', textX, startY + (imageWidth / 2) - 13.5); // Ajuste para centrar verticalmente el texto con la imagen
+        // const textX = startX;
+        // doc.fontSize(27).text('Factura Guardog', textX, startY + (imageWidth / 2) - 13.5); // Ajuste para centrar verticalmente el texto con la imagen
 
-        // Añadir tabla de datos ficticios de una factura
-        const tableTop = startY + 150; // Posición Y inicial de la tabla
-        const itemX = startX;
-        const descriptionX = startX + 150;
-        const unitCostX = startX + 300;
-        const quantityX = startX + 400;
-        const lineTotalX = startX + 500;
+        doc.fontSize(27).text('Factura Guardog', 100, 100); // Ajuste para centrar verticalmente el texto con la imagen
 
-        // Encabezados de la tabla
-        doc.fontSize(12)
-            .text('Item', itemX, tableTop)
-            .text('Description', descriptionX, tableTop)
-            .text('Unit Cost', unitCostX, tableTop)
-            .text('Quantity', quantityX, tableTop)
-            .text('Line Total', lineTotalX, tableTop);
 
-        // Datos ficticios de la tabla
-        const items = [
-            { item: '1', description: 'Producto A', unitCost: '10.00', quantity: '2', lineTotal: '20.00' },
-            { item: '2', description: 'Producto B', unitCost: '15.00', quantity: '1', lineTotal: '15.00' },
-            { item: '3', description: 'Producto C', unitCost: '7.50', quantity: '3', lineTotal: '22.50' },
-            { item: '4', description: 'Producto D', unitCost: '12.00', quantity: '1', lineTotal: '12.00' }
-        ];
 
-        let yPosition = tableTop + 20;
+        // // Añadir tabla de datos ficticios de una factura
+        // const tableTop = startY + 150; // Posición Y inicial de la tabla
+        // const itemX = startX;
+        // const descriptionX = startX + 150;
+        // const unitCostX = startX + 300;
+        // const quantityX = startX + 400;
+        // const lineTotalX = startX + 500;
 
-        items.forEach(item => {
-            doc.text(item.item, itemX, yPosition)
-                .text(item.description, descriptionX, yPosition)
-                .text(item.unitCost, unitCostX, yPosition)
-                .text(item.quantity, quantityX, yPosition)
-                .text(item.lineTotal, lineTotalX, yPosition);
-            yPosition += 20; // Incrementar posición Y para la siguiente fila
-        });
+        // // Encabezados de la tabla
+        // doc.fontSize(12)
+        //     .text('Item', itemX, tableTop)
+        //     .text('Description', descriptionX, tableTop)
+        //     .text('Unit Cost', unitCostX, tableTop)
+        //     .text('Quantity', quantityX, tableTop)
+        //     .text('Line Total', lineTotalX, tableTop);
 
-        // Añadir línea al final del documento
-        const bottomLineY = doc.page.height - 100;
-        doc.rect(0, bottomLineY, doc.page.width, 100).fill('#f0f0f0');
+        // // Datos ficticios de la tabla
+        // const items = [
+        //     { item: '1', description: 'Producto A', unitCost: '10.00', quantity: '2', lineTotal: '20.00' },
+        //     { item: '2', description: 'Producto B', unitCost: '15.00', quantity: '1', lineTotal: '15.00' },
+        //     { item: '3', description: 'Producto C', unitCost: '7.50', quantity: '3', lineTotal: '22.50' },
+        //     { item: '4', description: 'Producto D', unitCost: '12.00', quantity: '1', lineTotal: '12.00' }
+        // ];
 
-        // Añadir texto de agradecimiento dentro del rectángulo
-        const thankYouText = 'Gracias por su confianza';
-        const textXPosition = doc.page.width / 2;
-        const textYPosition = bottomLineY + 50;
+        // let yPosition = tableTop + 20;
 
-        doc.fillColor('black')
-            .fontSize(20)
-            .text(thankYouText, textXPosition, textYPosition, {
-                width: doc.page.width,
-                align: 'center'
-            });
+        // items.forEach(item => {
+        //     doc.text(item.item, itemX, yPosition)
+        //         .text(item.description, descriptionX, yPosition)
+        //         .text(item.unitCost, unitCostX, yPosition)
+        //         .text(item.quantity, quantityX, yPosition)
+        //         .text(item.lineTotal, lineTotalX, yPosition);
+        //     yPosition += 20; // Incrementar posición Y para la siguiente fila
+        // });
+
+        // // Añadir línea al final del documento
+        // const bottomLineY = doc.page.height - 100;
+        // doc.rect(0, bottomLineY, doc.page.width, 100).fill('#f0f0f0');
+
+        // // Añadir texto de agradecimiento dentro del rectángulo
+        // const thankYouText = 'Gracias por su confianza';
+        // const textXPosition = doc.page.width / 2;
+        // const textYPosition = bottomLineY + 50;
+
+        // doc.fillColor('black')
+        //     .fontSize(20)
+        //     .text(thankYouText, textXPosition, textYPosition, {
+        //         width: doc.page.width,
+        //         align: 'center'
+        //     });
 
         // Terminar el documento PDF
         doc.end();
